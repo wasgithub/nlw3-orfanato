cceb86bf4c0e3311e9e76899f4c84d4fa7a101b2
Washington Alexandre da Silva <was.alexandre42@gmail.com>
2021-06-10 10:21:25 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import 'express-async-errors';
+
+import './database/connection';
+
+import routes from './routes'
+import errorHandler from './errors/handler';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
+
+// Rota que a aplicação estará hospedada
+app.listen(3333, () => {
+  console.log("🚀 Server running on port 3333!");
+});
